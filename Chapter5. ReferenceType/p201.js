ed719f2cf795e0d133b1f35e4b29175dc17b2e46
안승훈 <size411@naver.com>
2021-04-24 17:47:42 +0900

    Repository Restore

--- Chapter5. ReferenceType/p201.js
+++ Chapter5. ReferenceType/p201.js
@@ -5,3 +5,8 @@ alert(encodeURI(uri));
 
 // "http%3A%2F%2Fwww.wrox.com%2Fillegal%20value.htm%23start"
 alert(encodeURIComponent(uri));
+
+// encodeURI()는 공백문자를 제외한 특수문자는 전혀 손대지 않고 공백문자만 %20로 인코드 한 반면 
+// encodeURIComponent() 메서드는 모든 특수문자를 인코드 했다.
+// 따라서 encodeURI()는 완전한 URI에서 사용할 수 있지만 encodeURIComponent()는
+// URI 마지막에 추가할 문자열에만 사용할 수 있다.
