8d3293c9b3be552e5c88b72f9f2c78486c02bf5a
안승훈 <size411@naver.com>
2021-05-05 20:05:02 +0900

    Closure Outline & Sample

---
+++ Chapter7. Function_Expression/p272.js
@@ -0,0 +1,32 @@
+function createComparisonFunction(propertyName) {
+	return function(object1, object2) {
+        // 익명 함수(내부 함수)에서 외부 함수의 변수 propertyName에 접근
+		var value1 = object1[propertyName];
+		var value2 = object2[propertyName];
+
+		if (value1 < value2) {
+			return -1;
+		}
+		else if (value1 > value2) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+}
+// 내부 함수의 스코프 체인에 createComparisonFunction()의 스코프가 포함되기 때문
+
+function compare(value1, value2) {
+	if (value1 < value2) {
+		return -1;
+	}
+	else if (value1 > value2) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+var result = compare(5, 10);
